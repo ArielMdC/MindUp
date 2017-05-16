fe37e8519c1599231a2258e3b78c54bf46b264f3
Fridilipi <Fridilipi>
2017-05-16 04:34:32 -0300

    ...

--- app/src/main/java/com/example/mindup/MainActivity.java
+++ app/src/main/java/com/example/mindup/MainActivity.java
@@ -1,11 +1,9 @@
-/*
-package com.mindup.ariel.mindup;
+package com.example.mindup;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -99,4 +98,3 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 }
-*/
